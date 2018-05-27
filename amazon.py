24114a00aabc3f22e5c343a71bed78377bfe08b2
MisaelMvM <porincrivelqpareca@gmail.com>
2018-05-27 14:15:59 -0400

    Data Clean Up

--- amazon.py
+++ amazon.py
@@ -6,37 +6,44 @@ import csv
 
 keywords = sys.argv
 url = 'https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Daps&field-keywords='
-f = csv.writer(open('amazon_results.csv', 'w'))
-# f.writerow(['Title', 'Price', 'Rate', 'Img_Url'])
 
-for idx, keyword in enumerate(keywords):
-	if idx != 0:
-		if idx+1 != len(keywords):
-			url = url + keyword + '+'
-		else:
-			url = url + keyword
+if len(keywords) > 1:
+	f = csv.writer(open('amazon_results.csv', 'w'))
+	f.writerow(['Title', 'Price', 'Rate', 'Img_Url'])
 
-source = get(url, headers={"User-Agent":"Defined"}).text
+	for idx, keyword in enumerate(keywords):
+		if idx != 0:
+			if idx+1 != len(keywords):
+				url = url + keyword + '+'
+			else:
+				url = url + keyword
 
-soup = BeautifulSoup(source, 'lxml')
+	source = get(url, headers={"User-Agent":"Defined"}).text
 
-# f.writerow(soup)
+	soup = BeautifulSoup(source, 'lxml')
 
-for item in soup.find_all('li', class_='s-result-item'):
-	if item['id'] != 's-result-list-layout-placeholder':
-		print(item['id'])
-		img_url = item.find('a', class_='a-link-normal a-text-normal').img['src']
-		
-		title = item.find('h2', class_='s-access-title').text
-		
-		price = str(item.find('span', class_='sx-price-whole').text) + ',' + str(item.find('sup', class_='sx-price-fractional').text)
+	# f.writerow(soup)
 
-		rate = 'No Rate'
-		
-		if item.find('i', class_='a-icon-star'):
+	for item in soup.find_all('li', class_='s-result-item'):
+		if item['id'] != 's-result-list-layout-placeholder' and item.find('h2', class_='s-access-title') and item.find('a', class_='a-link-normal a-text-normal') and item.find('i', class_='a-icon-star'):
+			
+			title = item.find('h2', class_='s-access-title').text
+
+			img_url = item.find('a', class_='a-link-normal a-text-normal').img['src']
+
 			rate = item.find('i', class_='a-icon-star').span.text
 
-		f.writerow([item['id'], title, price, img_url, rate])
+			price = 'No Price'
+			
+			if item.find('span', class_='sx-price-whole'):
+				price = str(item.find('span', class_='sx-price-whole').text) 
+				if item.find('sup', class_='sx-price-fractional'):
+					price = price + ',' + str(item.find('sup', class_='sx-price-fractional').text)
+
+			f.writerow([title, price, img_url, rate])
+else:
+	print('Keywords are required!')
+	print('Please refer -> \'python amazon.py [keywords]\'')
 
 
 
